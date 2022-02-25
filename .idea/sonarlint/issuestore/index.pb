cd488c9bb8b7299271c6e4352fdd9c6a6c9d451e
Jijo A J <love4messi@gmail.com>
2022-02-25 12:05:24 +0530

    done

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,21 @@
 
 <
 practice.iml,1\9\19fe682b9f294595378fc9265f0eab49918635ef
+A
+src/Solution.java,e\6\e69bf9f9fa39dd11b637f1006bcf9ba6c739a9b6
+B
+src/Solution2.java,b\d\bdd18e2af5659023939dfee0d820668fa869110c
+B
+src/Solution3.java,6\f\6f409a1c31905b3b3ca320e52ae75f0cfa7ea82f
+B
+src/Solution4.java,b\0\b0e79339e08657d5ba08f7622a95b9e7a760df59
+B
+src/Solution5.java,7\7\7739ff0fc28c542557e5941df009352650f94145
+C
+src/LinkedList.java,0\0\00fd33ac798e066bff270304abba2030492c06ee
+I
+src/PerfectSubstring.java,8\6\8643fe957b659c12dccb61e8d087b899aadeb777
+J
+src/ArrayManipulation.java,f\1\f1dcfd23f1fe5ddcaf368f714b2ffd56f9667812
+D
+src/JobSchedule.java,4\7\470b05745e5f736c0ca812e3f55a83d12d72faf1
--- src/JobSchedule.java
+++ src/JobSchedule.java
@@ -9,7 +9,6 @@ public class JobSchedule {
     static int unknownCount = 0;
     static List<Integer> unknownPositions = new LinkedList<>();
 
-
     public static String replaceChar(char[] ch) {
         StringBuilder myString = new StringBuilder(initialString);
         for (int i = 0; i < unknownCount; i++) {
@@ -20,18 +19,14 @@ public class JobSchedule {
 
     // Recursive function to print all n-digit numbers
     // whose sum of digits equals to given sum
-
     // n, sum --> value of inputs
     // out --> output array
     // index --> index of next digit to be
     // filled in output array
-    static void findNDigitNumsUtil(int n, int sum, char[] out,
-                                   int index, int max) {
-
+    static void findNDigitNumsUtil(int n, int sum, char[] out, int index, int max) {
         // Base case
         if (index > n || sum < 0)
             return;
-
         // If number becomes N-digit
         if (index == n) {
             // if sum of its digits is equal to given sum,
@@ -42,13 +37,11 @@ public class JobSchedule {
             }
             return;
         }
-
         // Traverse through every digit. Note that
         // here we're considering leading 0's as digits
         for (int i = 0; i <= max; i++) {
             // append current digit to number
             out[index] = (char) (i + '0');
-
             // recurse for next digit with reduced sum
             findNDigitNumsUtil(n, sum - i, out, index + 1, max);
         }
@@ -59,7 +52,6 @@ public class JobSchedule {
     static void findNDigitNums(int n, int sum, int max) {
         // output array to store N-digit numbers
         char[] out = new char[n];
-
         // fill 1st position by every digit from 1 to 9 and
         // calls findNDigitNumsUtil() for remaining positions
         for (int i = 0; i <= max; i++) {
@@ -67,7 +59,6 @@ public class JobSchedule {
             findNDigitNumsUtil(n, sum - i, out, 1, max);
         }
     }
-
 
     public static List<String> findSchedules(int workHours, int dayHours, String pattern) {
         initialString = pattern;
@@ -89,7 +80,6 @@ public class JobSchedule {
             int balance = workHours - initialSum;
             findNDigitNums(unknownCount, balance, dayHours);
         }
-
         return resultArray;
     }
 
